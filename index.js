8b05eb79c072111fb334ef99c0d4c72314e4c69e
Inna Cervenkova <inna.cervenkova@gmail.com>
2023-03-26 17:29:24 +0200

    prace s objekty

---
+++ index.js
@@ -0,0 +1,104 @@
+const book1 = {
+  title: 'Harry Potter a kámen mudrců',
+  titleEnglish: "Harry Potter and the Philosopher's Stone",
+  author: {
+    firstName: 'Joanne',
+    lastName: 'Rowlingová',
+    dateOfBirth: '1965-31-07',
+  },
+  numberOfPages: 256,
+  ISBN: 9788000066967,
+  yearOfPublication: 2022,
+  publisher: 'Albatros',
+  movie: {
+    year: 2001,
+    productionCompany: 'Warner Bros.',
+    distribution: 'Warner Bros. Pictures',
+    director: {
+      firstName: 'David',
+      lastName: 'Heyman'
+    },
+    camera: {
+      firstName: 'John',
+      lastName: 'Seale',
+    },
+  },
+}
+
+
+const book2 = {
+  titleCzech: 'Pravidla moštárny',
+  titleEnglish: 'The Cider House Rules',
+  author: {
+    firstName: 'John',
+    lastName: 'Irving',
+    dateOfBirth: '1965-31-07',
+  },
+  numberOfPages: 622,
+  ISBN: 8020711783,
+  yearOfPublication: 2005,
+  publisher: 'ODEON',
+  movie: {
+    year: 2000,
+    productionCompany: 'FilmColony',
+    distribution: 'Miramax Films',
+    director: {
+      firstName: 'Richard',
+      lastName: 'Gladstein'
+    },
+    camera: {
+      firstName: 'Oliver',
+      lastName: 'Stapleton',
+    },
+  },
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+const book = {
+  title: 'Lord of the Rings',
+  author: {
+    name: 'John Ronald Reuel',
+    surename: 'Tolkien',
+    age: 81,
+  },
+  numberOfPages: 2057,
+  movie: {
+    year: 2000,
+    director: {
+      name: 'Peter',
+      surname: 'Jackson',
+    },
+    actors: {
+      actor1: {
+        name: 'Orlando',
+        surname: 'Bloom',
+        characterName: 'Legolas',
+      },
+      actor2: {
+        name: 'Elijah',
+        surname: 'Wood',
+        characterName: 'Frodo',
+      },
+      actor3: {
+        name: 'Viggo',
+        surname: 'Mortensen',
+        characterName: 'Aragorn',
+      },
+    },
+  },
+};
+*/
